2cf17269cbd4b34e6daf818b7b6b0e046df5d9b4
djensenius <djensenius@wesleyan.edu>
2013-05-05 01:36:16 -0400

    Add user commit tracking
    Signed-off-by: djensenius <djensenius@wesleyan.edu>

--- config-sample.php
+++ config-sample.php
@@ -4,8 +4,12 @@
 // https://help.github.com/articles/creating-an-oauth-token-for-command-line-use
 define('AUTH_TOKEN', 'your_token');
 
-// Define the type of graph for repository commit's. Options are bar and line
+// Define the type of graph for repository commit's. Options are bar and line.
 define('REPOSITORY_COMMIT_GRAPH', 'line');
+
+// Define the type of graph for user commit's. Options are bar and line.
+// The user commits currently searches through the repositories specified below.
+define('USER_COMMIT_GRAPH', 'bar');
 
 // Define the repositories you wish to track.
 $repositories = array(
---
+++ user-commit-graph.php
@@ -0,0 +1,39 @@
+<?php
+require_once('config.php');
+require_once 'php-github-api/vendor/autoload.php';
+
+$client = new Github\Client();
+if (defined('AUTH_TOKEN') && AUTH_TOKEN != 'your_token') {
+	$client->authenticate(AUTH_TOKEN, '', Github\Client::AUTH_HTTP_TOKEN);
+}
+
+$sevendaysago = date('Y-m-d', strtotime('-7 days')) . 'T00:00:00+00:00';
+
+$graph['graph']['title'] = "User Commits";
+$graph['graph']['type'] = USER_COMMIT_GRAPH;
+
+foreach ($repositories as $repository) {
+	list($username, $repo, $branch) = explode('/', $repository['repo']);
+	$name = $repository['name'];
+	error_log("Looping through $name");
+	$commits = $client->api('repo')->commits()->all($username, $repo, array('sha' => $branch, 'since' => $sevendaysago));
+	foreach ($commits as $commit) {
+		if (isset($commit['commit']['author']['name'])) {
+			$username = $commit['commit']['author']['name'];
+			if (isset($commitname[$username])) {
+				$commitname[$username]++;
+				
+			} else {
+				$commitname[$username] = 1;
+			}
+		}
+	}	
+}
+
+foreach ($commitname as $name=>$commits) {
+	$datapoints = array("value"=>$commits, "title"=>$name);
+	$graph['graph']['datasequences'][] = array("title"=>$name, "datapoints"=>array($datapoints));
+}
+
+print json_encode($graph) . "\n";
+?>
